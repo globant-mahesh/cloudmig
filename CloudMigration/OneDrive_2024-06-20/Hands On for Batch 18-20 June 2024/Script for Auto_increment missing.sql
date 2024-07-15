3dd3caef28e4e32acfff8a19da6f6e1115f879f6
Mahesh Bhosale <bhosal.mahesh@gm.com>
2024-07-15 20:43:48 +0530

    cloud data migration

---
+++ CloudMigration/OneDrive_2024-06-20/Hands On for Batch 18-20 June 2024/Script for Auto_increment missing.sql
@@ -0,0 +1,8 @@
+ALTER TABLE college_mgmt.tbl_student 
+  MODIFY id INT(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE college_mgmt.tbl_users 
+  MODIFY user_id INT(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE college_mgmt.tbl_college 
+  MODIFY college_id INT(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE college_mgmt.tbl_roles 
+  MODIFY role_id INT(11) NOT NULL AUTO_INCREMENT;
